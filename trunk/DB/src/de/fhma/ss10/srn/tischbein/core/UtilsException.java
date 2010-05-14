4bb183e2e6600dbde78e6420cdb284ac4926f493
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-14 18:43:15 +0000

    UserException hinzugefügt

---
+++ trunk/DB/src/de/fhma/ss10/srn/tischbein/core/UtilsException.java
@@ -0,0 +1,9 @@
+package de.fhma.ss10.srn.tischbein.core;
+
+public class UtilsException extends Exception {
+
+    public UtilsException(final String message, final Exception e) {
+        super(message, e);
+    }
+
+}
