e496edec7b60865dbc9200dcca8c0d5ad71e9e44
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-14 17:19:33 +0000

    Paar Änderungen an die Verschobene Klasse User

--- GUI/src/de/fhma/ss10/srn/tischbein/gui/MainFrame.java
+++ GUI/src/de/fhma/ss10/srn/tischbein/gui/MainFrame.java
@@ -13,8 +13,8 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-import de.fhma.ss10.srn.tischbein.core.User;
 import de.fhma.ss10.srn.tischbein.core.db.Database;
+import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.actions.LoginAction;
 import de.fhma.ss10.srn.tischbein.gui.actions.NewUserAction;
 
