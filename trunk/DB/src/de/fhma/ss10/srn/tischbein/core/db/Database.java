5f1cd25692005850ca71ae7f60922ec13cd131d4
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-14 17:22:18 +0000

    Kommentare und umsortiert und so

--- trunk/DB/src/de/fhma/ss10/srn/tischbein/core/db/Database.java
+++ trunk/DB/src/de/fhma/ss10/srn/tischbein/core/db/Database.java
@@ -1,18 +1,11 @@
 package de.fhma.ss10.srn.tischbein.core.db;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.security.InvalidKeyException;
 import java.util.TreeMap;
 import java.util.Vector;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-
-import de.fhma.ss10.srn.tischbein.core.User;
 import de.fhma.ss10.srn.tischbein.core.Utils;
 
 /**
@@ -26,102 +19,111 @@ public final class Database {
     private static final char SEPARATOR = ';';
     private static Database instance = null;
 
-    private final TreeMap<String, User> users;
-    private String rootPath;
+    private final TreeMap<String, User> users = new TreeMap<String, User>();
 
+    /**
+     * Privater ctor, um die Instanziierung außerhalb zu verhindern.
+     */
     private Database() {
-        this.users = new TreeMap<String, User>();
     }
 
     public static synchronized Database getInstance() {
         if (Database.instance == null) {
             try {
-                Database.instance = Database.open("/");
-            } catch (IOException e) {
+                Database.instance = Database.open();
+            } catch (Exception e) {
                 e.printStackTrace();
 
-                Database.instance = null;
+                throw new RuntimeException("FATAL: Kann keine Instanz der Datenbank erzeugen!", e);
             }
         }
 
         return Database.instance;
     }
 
-    private static Database open(final String root) throws IOException {
-        Database db = new Database();
-        File path = new File(root);
+    private static Database open() throws DatabaseException {
+        try {
+            Database db = new Database();
 
-        if (!path.isDirectory()) {
-            throw new IllegalArgumentException("Root path argument must be a directory!");
-        }
+            db.loadUsers();
+            //      db.  loadFiles();
 
-        db.setRootDir(root);
-
-        db.loadUsers();
-        //      db.  loadFiles();
-
-        return db;
-    }
-
-    private void loadUsers() throws IOException {
-        BufferedReader fr = new BufferedReader(new FileReader(Database.DB_USERS_TB));
-        String line;
-
-        while ((line = fr.readLine()) != null) {
-            User user = User.read(line);
-
-            this.users.put(user.getName().toLowerCase(), user);
+            return db;
+        } catch (Exception e) {
+            throw new DatabaseException("Kann die Datenbankstruktur nicht laden!", e);
         }
     }
 
-    private void setRootDir(final String root) {
-        this.rootPath = root;
+    private void loadUsers() throws DatabaseException {
+        try {
+            BufferedReader fr = new BufferedReader(new FileReader(Database.DB_USERS_TB));
+            String line;
+
+            while ((line = fr.readLine()) != null) {
+                User user = User.parse(line);
+
+                this.users.put(user.getName().toLowerCase(), user);
+            }
+
+            fr.close();
+        } catch (Exception e) {
+            throw new DatabaseException("Kann die Benutzertabelle nicht laden!", e);
+        }
     }
 
-    public void createUser(final String name, final String pass) throws InvalidKeyException, IllegalBlockSizeException,
-            BadPaddingException, IOException {
-        if (this.users.containsKey(name.toLowerCase())) {
-            throw new IllegalArgumentException("Benutzer existiert schon!");
+    public void createUser(final String name, final String pass) throws DatabaseException {
+        try {
+            if (this.users.containsKey(name.toLowerCase())) {
+                throw new DatabaseException("Benutzer existiert schon!");
+            }
+
+            User user = User.create(name, pass);
+
+            this.saveToUsers(user, pass);
+        } catch (Exception e) {
+            throw new DatabaseException("Fehler beim Anlegen des neuen Benutzers!", e);
         }
-
-        User user = User.create(name, pass);
-
-        this.saveToUsers(user, pass);
     }
 
-    private void saveToUsers(final User user, final String pass) throws IOException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException {
-        FileWriter fw = new FileWriter(Database.DB_USERS_TB, true);
-        StringBuilder userLine = new StringBuilder();
+    private void saveToUsers(final User user, final String pass) throws DatabaseException {
+        try {
+            FileWriter fw = new FileWriter(Database.DB_USERS_TB, true);
+            StringBuilder userLine = new StringBuilder();
 
-        userLine.append(user.getName());
-        userLine.append(Database.SEPARATOR);
-        userLine.append(user.getPassHash());
-        userLine.append(Database.SEPARATOR);
-        userLine.append(Utils.toHexString(user.getPublicKey().getEncoded()));
-        userLine.append(Database.SEPARATOR);
-        userLine.append(Utils.toHexString(Utils.encrypt(user.getPrivateKey().getEncoded(), pass)));
+            userLine.append(user.getName());
+            userLine.append(Database.SEPARATOR);
+            userLine.append(user.getPassHash());
+            userLine.append(Database.SEPARATOR);
+            userLine.append(Utils.toHexString(user.getPublicKey().getEncoded()));
+            userLine.append(Database.SEPARATOR);
+            userLine.append(Utils.toHexString(Utils.encrypt(user.getPrivateKey().getEncoded(), pass)));
 
-        fw.append(userLine);
-        fw.append("\n");
-        fw.flush();
+            fw.append(userLine);
+            fw.append("\n");
+            fw.flush();
 
-        fw.close();
+            fw.close();
+        } catch (Exception e) {
+            throw new DatabaseException("Fehler beim Schreiben in die Users-Tabelle!", e);
+        }
     }
 
-    public User loginUser(final String name, final String pass) throws InvalidKeyException, IllegalBlockSizeException,
-            BadPaddingException {
-        User user = this.users.get(name.toLowerCase());
+    public User loginUser(final String name, final String pass) throws DatabaseException {
+        try {
+            User user = this.users.get(name.toLowerCase());
 
-        if (user == null) {
-            throw new IllegalArgumentException("Benutzer existiert nicht!");
+            if (user == null) {
+                throw new DatabaseException("Benutzer existiert nicht!");
+            }
+
+            if (!user.unlock(pass)) {
+                throw new DatabaseException("Falsches Passwort!");
+            }
+
+            return user;
+        } catch (Exception e) {
+            throw new DatabaseException("Fehler beim Einloggen!", e);
         }
-
-        if (!user.unlock(pass)) {
-            throw new IllegalArgumentException("Falsches Passwort!");
-        }
-
-        return user;
     }
 
     public Vector<String> getUserList(final User user) {
