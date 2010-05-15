456dc274f73b2344e5615bba4a9b9dfdfaa215b4
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-15 11:32:05 +0000

    Kommentare

--- trunk/DB/src/de/fhma/ss10/srn/tischbein/core/UtilsException.java
+++ trunk/DB/src/de/fhma/ss10/srn/tischbein/core/UtilsException.java
@@ -1,10 +1,23 @@
 package de.fhma.ss10.srn.tischbein.core;
 
+/**
+ * Kapselnde Exception um den Wust an möglichen Exceptions in der Utils-Klasse zu mindern.
+ * 
+ * @author Smolli
+ */
 public class UtilsException extends Exception {
 
-    /**Serial UID.     */
+    /** Serial UID. */
     private static final long serialVersionUID = 4687628517898336248L;
 
+    /**
+     * Erstellt eine neue UtilityException.
+     * 
+     * @param message
+     *            Die Nachricht.
+     * @param e
+     *            Die verursachende Exception.
+     */
     public UtilsException(final String message, final Exception e) {
         super(message, e);
     }
