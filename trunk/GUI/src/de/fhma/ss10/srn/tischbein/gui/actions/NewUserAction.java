317c4b60f9c6e231bf64fce0339af728af788226
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-15 11:15:09 +0000

    Kommentare

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/NewUserAction.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/NewUserAction.java
@@ -10,15 +10,32 @@ import javax.swing.JTextField;
 
 import de.fhma.ss10.srn.tischbein.core.db.Database;
 
-public class NewUserAction extends AbstractAction {
+/**
+ * Action zum Anlegen eines neuen Benutzers.
+ * 
+ * @author Smolli
+ */
+public final class NewUserAction extends AbstractAction {
 
+    /** Serial UID. */
     private static final long serialVersionUID = -6210538596713120230L;
+
+    /** Hält den Benutzernamen. */
     private final JTextField username;
+    /** Hält das Benutzerpasswort. */
     private final JPasswordField userpass;
 
-    public NewUserAction(final JTextField username, final JPasswordField userpass) {
-        this.username = username;
-        this.userpass = userpass;
+    /**
+     * Erstellt eine neue "Benutzer anlegen"-Action.
+     * 
+     * @param nameField
+     *            Das {@link JTextField} mit dem Benutzernamen.
+     * @param passField
+     *            Das {@link JPasswordField} mit dem Benutzerpasswort.
+     */
+    public NewUserAction(final JTextField nameField, final JPasswordField passField) {
+        this.username = nameField;
+        this.userpass = passField;
     }
 
     @Override
