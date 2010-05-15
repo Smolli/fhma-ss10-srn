64fb7a4878a563944e14ddc5bb1dce2fa44c82a1
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-15 11:11:35 +0000

    Benutzername und -passwort an die Actions übergeben.

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/LoginAction.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/LoginAction.java
@@ -5,20 +5,36 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import de.fhma.ss10.srn.tischbein.core.db.Database;
 import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.launcher.Launcher;
 
-public class LoginAction extends AbstractAction {
+/**
+ * Action zum einloggen eines Benutzers.
+ * 
+ * @author Smolli
+ */
+public final class LoginAction extends AbstractAction {
+
+    /** Serial UID. */
+    private static final long serialVersionUID = 4294231070983688689L;
+
+    private final JTextField username;
+    private final JPasswordField userpass;
+
+    public LoginAction(final JTextField username, final JPasswordField userpass) {
+        this.username = username;
+        this.userpass = userpass;
+    }
 
     @Override
     public void actionPerformed(final ActionEvent arg0) {
-        String name = Launcher.getFrame().getUsername().getText();
-        String pass = new String(Launcher.getFrame().getUserpass().getPassword());
-
         try {
-            User user = Database.getInstance().loginUser(name, pass);
+            User user = Database.getInstance().loginUser(this.username.getText(),
+                    new String(this.userpass.getPassword()));
 
             Launcher.getFrame().setUser(user);
         } catch (Exception ex) {
