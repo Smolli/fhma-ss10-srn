4e985b9dd700c68b2445ad788ca895fa052ab8df
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-15 11:08:38 +0000

    Kommentare

--- DB/src/de/fhma/ss10/srn/tischbein/core/UtilsException.java
+++ DB/src/de/fhma/ss10/srn/tischbein/core/UtilsException.java
@@ -2,6 +2,9 @@ package de.fhma.ss10.srn.tischbein.core;
 
 public class UtilsException extends Exception {
 
+    /**Serial UID.     */
+    private static final long serialVersionUID = 4687628517898336248L;
+
     public UtilsException(final String message, final Exception e) {
         super(message, e);
     }
