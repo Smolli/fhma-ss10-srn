d43ead494643ae8a2318fb6845819e504dd4354d
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-15 11:58:47 +0000

    Kommentare

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/MainFrame.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/MainFrame.java
@@ -18,73 +18,156 @@ import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.actions.LoginAction;
 import de.fhma.ss10.srn.tischbein.gui.actions.NewUserAction;
 
-public class MainFrame extends JFrame {
+/**
+ * Hauptfenster.
+ * 
+ * @author Smolli
+ */
+public final class MainFrame extends JFrame {
 
+    /** Serial UID. */
     private static final long serialVersionUID = 5425989856036812026L;
 
-    private JPasswordField userpass;
-    private JTextField username;
+    /** Fensterhöhe. */
+    private static final int DEFAULT_APP_HEIGHT = 600;
+    /** Fensterbreite. */
+    private static final int DEFAULT_APP_WIDTH = 800;
 
+    /** Hält das Benutzerpasswort. */
+    private JPasswordField userpass;
+    /** Hält den Benutzernamen. */
+    private JTextField username;
+    /** Hält das Login-Panel. */
+    private Container loginPanel;
+    /** Östlicher Arbeitsplatzbereich. */
+    private Container wbEast;
+    /** Nördlicher Arbeitsplatzbereich. */
+    private Container wbNorth;
+    /** Südlicher Arbeitsplatzbereich. */
+    private Container wbSouth;
+    /** Hält das Benutzerobjekt. */
+    private User user;
+
+    /**
+     * Erstellt ein neues Hauptfenster.
+     */
     public MainFrame() {
         this.initFrame();
 
-        this.initComponents();
+        this.initLoginView();
     }
 
+    /**
+     * Setzt den eingeloggten Benutzer und schaltet die Arbeitsansicht frei.
+     * 
+     * @param newUser
+     *            Der Benutzer.
+     */
+    public void setUser(final User newUser) {
+        this.user = newUser;
 
-    public void setUser(final User user) {
-        this.getContentPane().removeAll();
-
-        this.createWorkbench(user);
-
-        this.getContentPane().repaint();
-        this.getContentPane().validate();
+        this.initWorkbenchView();
     }
 
-    private Container createLoginPanel() {
+    /**
+     * Erstellt das Login-Panel.
+     */
+    private void createLoginPanel() {
         GridLayout layout = new GridLayout(4, 2);
-        JPanel loginPanel = new JPanel();
+        this.loginPanel = new JPanel();
 
-        loginPanel.setLayout(layout);
+        this.userpass = new JPasswordField();
+        this.username = new JTextField();
 
-        loginPanel.add(new JLabel("Tischbein"));
-        loginPanel.add(new JLabel("Login"));
+        this.loginPanel.setLayout(layout);
 
-        loginPanel.add(new JLabel("Benutzername:"));
-        loginPanel.add(this.username = new JTextField());
+        this.loginPanel.add(new JLabel("Tischbein"));
+        this.loginPanel.add(new JLabel("Login"));
 
-        loginPanel.add(new JLabel("Passwort:"));
-        loginPanel.add(this.userpass = new JPasswordField());
+        this.loginPanel.add(new JLabel("Benutzername:"));
+        this.loginPanel.add(this.username);
 
-        loginPanel.add(new JButton(new LoginAction(this.username, this.userpass)));
-        loginPanel.add(new JButton(new NewUserAction(this.username, this.userpass)));
+        this.loginPanel.add(new JLabel("Passwort:"));
+        this.loginPanel.add(this.userpass);
 
-        return loginPanel;
+        this.loginPanel.add(new JButton(new LoginAction(this.username, this.userpass)));
+        this.loginPanel.add(new JButton(new NewUserAction(this.username, this.userpass)));
     }
 
-    private void createWorkbench(final User user) {
+    /**
+     * Erstellt das Arbeitsplatz-Panel.
+     */
+    private void createWorkbench() {
+        this.createWorkbenchEast();
+        this.createWorkbenchSouth();
+        this.createWorkbenchNorth();
+    }
+
+    /**
+     * Erstellt den östliche Arbeitsplatzbereich.
+     */
+    private void createWorkbenchEast() {
+        this.wbEast = new JPanel();
         JList list = new JList(Database.getInstance().getUserList());
 
-        this.getContentPane().add(list, BorderLayout.EAST);
-
-        this.getContentPane().add(new JButton("Logout"), BorderLayout.SOUTH);
-
-        this.getContentPane().add(new JLabel("Tischbein v0.1"), BorderLayout.NORTH);
+        this.wbEast.add(list);
     }
 
-    private void initComponents() {
-        BorderLayout layout = new BorderLayout();
+    /**
+     * Erstellt den nördlichen Arbeitsplatzbereich.
+     */
+    private void createWorkbenchNorth() {
+        this.wbNorth = new JPanel();
 
-        this.getContentPane().setLayout(layout);
-
-        this.getContentPane().add(this.createLoginPanel(), BorderLayout.CENTER);
+        this.wbNorth.add(new JLabel("Tischbein v0.1"));
     }
 
+    /**
+     * Erstellt den südlichen Arbeitsplatzbereich.
+     */
+    private void createWorkbenchSouth() {
+        this.wbSouth = new JPanel();
+
+        this.wbSouth.add(new JButton("Logout"));
+    }
+
+    /**
+     * Initialisiert das Fenster.
+     */
     private void initFrame() {
         this.setTitle("Tischbein v0.1");
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        this.setSize(800, 600);
+        this.setSize(MainFrame.DEFAULT_APP_WIDTH, MainFrame.DEFAULT_APP_HEIGHT);
+    }
+
+    /**
+     * Initialisiert die Login-Ansicht und zeigt sie an.
+     */
+    private void initLoginView() {
+        BorderLayout layout = new BorderLayout();
+
+        this.getContentPane().setLayout(layout);
+
+        this.createLoginPanel();
+
+        this.getContentPane().add(this.loginPanel, BorderLayout.CENTER);
+    }
+
+    /**
+     * Initialisiert die Arbeitsplatz-Ansicht und zeigt sie an.
+     */
+    private void initWorkbenchView() {
+        this.getContentPane().removeAll();
+
+        this.createWorkbench();
+
+        this.getContentPane().add(this.wbNorth, BorderLayout.NORTH);
+        this.getContentPane().add(this.wbEast, BorderLayout.EAST);
+        this.getContentPane().add(this.wbSouth, BorderLayout.SOUTH);
+
+        this.getContentPane().repaint();
+        this.getContentPane().validate();
     }
 
 }
