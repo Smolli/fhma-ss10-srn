760444a5b9ce401bc039c904bf941949460c2332
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-16 09:53:52 +0000

    CryptoException hinzugefügt

---
+++ DB/src/de/fhma/ss10/srn/tischbein/core/crypto/CryptoException.java
@@ -0,0 +1,9 @@
+package de.fhma.ss10.srn.tischbein.core.crypto;
+
+public class CryptoException extends Exception {
+
+    public CryptoException(final String message, final Exception e) {
+        super(message, e);
+    }
+
+}
