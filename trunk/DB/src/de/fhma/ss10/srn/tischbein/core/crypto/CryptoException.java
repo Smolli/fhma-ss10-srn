8ce926fb0dfac8544a7630ffd7db5915c3eaf7b9
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-16 10:08:57 +0000

    Kommentare

--- trunk/DB/src/de/fhma/ss10/srn/tischbein/core/crypto/CryptoException.java
+++ trunk/DB/src/de/fhma/ss10/srn/tischbein/core/crypto/CryptoException.java
@@ -1,7 +1,23 @@
 package de.fhma.ss10.srn.tischbein.core.crypto;
 
+/**
+ * Exception für das crypto-Package.
+ * 
+ * @author Smolli
+ */
 public class CryptoException extends Exception {
 
+    /** Serial UID. */
+    private static final long serialVersionUID = -2578760367264974049L;
+
+    /**
+     * Erstellt eine neue CryptoException.
+     * 
+     * @param message
+     *            Die Nachricht.
+     * @param e
+     *            Der Grund.
+     */
     public CryptoException(final String message, final Exception e) {
         super(message, e);
     }
