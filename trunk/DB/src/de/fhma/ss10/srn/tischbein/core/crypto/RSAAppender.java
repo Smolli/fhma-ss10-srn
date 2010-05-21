18290fc87651c5fb895128a628f10adca8e5842a
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 17:45:01 +0000

    RSAAppender auf Writer umgestellt

--- trunk/DB/src/de/fhma/ss10/srn/tischbein/core/crypto/RSAAppender.java
+++ trunk/DB/src/de/fhma/ss10/srn/tischbein/core/crypto/RSAAppender.java
@@ -1,21 +1,23 @@
 package de.fhma.ss10.srn.tischbein.core.crypto;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.security.PublicKey;
 
+import de.fhma.ss10.srn.tischbein.core.Utils;
 import de.fhma.ss10.srn.tischbein.core.UtilsException;
 
 public class RSAAppender {
 
-    public static void appendLine(final String filename, final byte[] publicKey, final String line)
+    public static void appendLine(final String filename, final PublicKey publicKey, final String line)
             throws UtilsException, IOException {
-        BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(new File(filename), true));
-        byte[] encoded = RsaCrypto.encode(line.getBytes(), publicKey);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename), true));
+        byte[] encoded = RsaCrypto.encode(line, publicKey);
 
-        writer.write(encoded);
-        writer.write("\n".getBytes());
+        writer.write(Utils.toHexString(encoded));
+        writer.write("\n");
 
         writer.close();
     }
