97f3d1601e38bb95ab130c358e5fa2b04c661ae1
kaczmarek.christian <kaczmarek.christian@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 12:12:00 +0000

    UploadFrame angelegt

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import de.fhma.ss10.srn.tischbein.core.db.User;
-import de.fhma.ss10.srn.tischbein.gui.actions.SearchAction;
 import de.fhma.ss10.srn.tischbein.gui.forms.UploadForm;
 
 public class UploadFrame extends UploadForm implements ActionListener {
