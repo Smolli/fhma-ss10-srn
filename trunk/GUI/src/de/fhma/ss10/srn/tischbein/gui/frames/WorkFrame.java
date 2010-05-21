e4011c4b51f2122518f35c5aa2487333bb85c268
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 14:52:36 +0000

    Listenverarbeitung hinzugefügt

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/WorkFrame.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/WorkFrame.java
@@ -1,9 +1,14 @@
 package de.fhma.ss10.srn.tischbein.gui.frames;
 
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.util.Vector;
+
+import javax.swing.DefaultListSelectionModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
 import de.fhma.ss10.srn.tischbein.core.db.Database;
+import de.fhma.ss10.srn.tischbein.core.db.FileItem;
 import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.actions.CloseAction;
 import de.fhma.ss10.srn.tischbein.gui.actions.DeleteAction;
@@ -18,48 +23,162 @@ import de.fhma.ss10.srn.tischbein.gui.forms.WorkForm;
  * 
  * @author Smolli
  */
-public final class WorkFrame extends WorkForm implements CloseActionListener,
-		LogoutActionListener {
+public final class WorkFrame extends WorkForm implements CloseActionListener, LogoutActionListener {
 
-	/** Serial UID. */
-	private static final long serialVersionUID = -5369888389274792872L;
+    /** Serial UID. */
+    private static final long serialVersionUID = -5369888389274792872L;
 
-	/** Hält den eingeloggten Benutzer. */
-	private final User user;
+    /** Hält den eingeloggten Benutzer. */
+    private final User user;
+    private FileItem selectedFile = null;
 
-	/**
-	 * Erstellt ein neues Arbeitsfenster mit dem übergebenen Benutzer.
-	 * 
-	 * @param newUser
-	 *            Der Benutzer.
-	 */
-	public WorkFrame(final User newUser) {
-		this.user = newUser;
-		this.closeButton.setAction(new CloseAction(this));
-		this.logoutButton.setAction(new LogoutAction(this));
-		this.uploadButton.setAction(new UploadAction(newUser));
-		this.deleteButton.setAction(new DeleteAction());
+    /**
+     * Erstellt ein neues Arbeitsfenster mit dem übergebenen Benutzer.
+     * 
+     * @param newUser
+     *            Der Benutzer.
+     */
+    public WorkFrame(final User newUser) {
+        this.user = newUser;
+        this.closeButton.setAction(new CloseAction(this));
+        this.logoutButton.setAction(new LogoutAction(this));
+        this.uploadButton.setAction(new UploadAction(newUser));
+        this.deleteButton.setAction(new DeleteAction());
 
-		this.myFilesList.setListData(this.user.getFileListObject()
-				.getFileList());
+        this.myFilesLlist.setListData(this.user.getFileListObject().getFileList());
 
-		this.setVisible(true);
-	}
+        this.setVisible(true);
 
-	@Override
-	public void close() {
-		Database.getInstance().shutdown();
+        this.userTable.setModel(this.generateUserModel());
+        //        this.otherFilesList.setListData(this.user.getFileListObject().getAccessList());
 
-		this.dispose();
-	}
+        this.myFilesLlist.setSelectionModel(new DefaultListSelectionModel() {
+            {
+                this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            }
 
-	@Override
-	public void logout() {
-		this.user.lock();
+            @Override
+            public void setSelectionInterval(final int first, final int last) {
+                super.setSelectionInterval(first, last);
 
-		new LoginFrame();
-		
-		this.close();
-	}
+                WorkFrame.this.selectedFile = WorkFrame.this.user.getFileListObject().getFileList().get(last);
 
+                System.out.println(WorkFrame.this.selectedFile + " wurde ausgewählt");
+            }
+
+        });
+    }
+
+    @Override
+    public void close() {
+        Database.getInstance().shutdown();
+
+        this.dispose();
+    }
+
+    @Override
+    public void logout() {
+        this.user.lock();
+
+        new LoginFrame();
+
+        this.close();
+    }
+
+    private TableModel generateUserModel() {
+
+        return new TableModel() {
+            Vector<User> users;
+
+            {
+                this.users = Database.getInstance().getUsers(WorkFrame.this.user);
+            }
+
+            @Override
+            public void addTableModelListener(final TableModelListener l) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public Class<?> getColumnClass(final int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                        return Boolean.class;
+
+                    case 1:
+                        return String.class;
+
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getColumnCount() {
+                return 2;
+            }
+
+            @Override
+            public String getColumnName(final int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                        return "";
+
+                    case 1:
+                        return "Benutzername";
+
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getRowCount() {
+                return this.users.size();
+            }
+
+            @Override
+            public Object getValueAt(final int rowIndex, final int columnIndex) {
+                User user = this.users.get(rowIndex);
+
+                switch (columnIndex) {
+                    case 0:
+                        return WorkFrame.this.user.getFileListObject().hasAccess(user, WorkFrame.this.selectedFile);
+
+                    case 1:
+                        return user.getName();
+
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public boolean isCellEditable(final int rowIndex, final int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                        return true;
+
+                    case 1:
+                        return false;
+
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public void removeTableModelListener(final TableModelListener l) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void setValueAt(final Object aValue, final int rowIndex, final int columnIndex) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+    }
 }
