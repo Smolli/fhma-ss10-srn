4159316d29c5163244c31469e22eecab09bfcb9d
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 19:19:06 +0000

    Cleanup

--- DB/src/de/fhma/ss10/srn/tischbein/core/db/Database.java
+++ DB/src/de/fhma/ss10/srn/tischbein/core/db/Database.java
@@ -1,8 +1,10 @@
 package de.fhma.ss10.srn.tischbein.core.db;
 
+import java.io.IOException;
 import java.util.Vector;
 
 import de.fhma.ss10.srn.tischbein.core.Utils;
+import de.fhma.ss10.srn.tischbein.core.UtilsException;
 import de.fhma.ss10.srn.tischbein.core.crypto.AESWriter;
 import de.fhma.ss10.srn.tischbein.core.crypto.RSAAppender;
 import de.fhma.ss10.srn.tischbein.core.db.FileListObject.UserFilePair;
@@ -41,6 +43,13 @@ public final class Database extends DatabaseModel {
         } finally {
             DatabaseStructure.LOCK.unlock();
         }
+    }
+
+    /**
+     * Setzt die Datenbankinstanz auf <code>null</code>.
+     */
+    private static void killInstance() {
+        Database.instance = null;
     }
 
     /**
@@ -97,7 +106,7 @@ public final class Database extends DatabaseModel {
         DatabaseStructure.LOCK.lock();
 
         try {
-            if (this.users.containsKey(name.toLowerCase())) {
+            if (this.getUserMap().containsKey(name.toLowerCase())) {
                 throw new DatabaseException("Benutzer existiert schon!");
             }
 
@@ -113,10 +122,22 @@ public final class Database extends DatabaseModel {
         }
     }
 
+    /**
+     * Gibt alle {@link User} in der Datenbank zurück.
+     * 
+     * @return Alle {@link User} als {@link Vector}.
+     */
     public Vector<User> getUsers() {
-        return new Vector<User>(this.users.values());
+        return new Vector<User>(this.getUserMap().values());
     }
 
+    /**
+     * Gibt alle Benutzer der Datenbank zurück, außer dem angegebenen Benutzer.
+     * 
+     * @param without
+     *            Der {@link User}, der nicht in der Liste auftauchen soll.
+     * @return Alle anderen Benutzer als {@link Vector}.
+     */
     public Vector<User> getUsers(final User without) {
         Vector<User> users = this.getUsers();
 
@@ -125,36 +146,41 @@ public final class Database extends DatabaseModel {
         return users;
     }
 
+    /**
+     * Erteilt dem übergebenen Benutzer Zugriffsrechte auf die übergebene Datei.
+     * 
+     * @param user
+     *            Der {@link User}, für den die Zugriffsrechte erteilt werden.
+     * @param file
+     *            Das {@link FileItem}, für das die Zugriffsrechte erteilt werden.
+     * @throws DatabaseException
+     *             Wird geworfen, wenn das Zugriffsrecht nicht erteilt werden konnte.
+     */
     public void grantAccess(final User user, final FileItem file) throws DatabaseException {
         if ((file == null) || (user == null)) {
             return;
         }
 
         try {
-            RSAAppender.appendLine(Tables.AccessTable.getFilename(user), user.getKeyPair().getPublic(), file.getId()
-                    + DatabaseModel.SEPARATOR + Utils.toHexString(file.getKey()));
+            this.appendToUser(user, file);
 
-            User owner = file.getOwner();
-            AESWriter w = AESWriter.createWriter(Tables.LendTable.getFilename(owner), owner.getCryptKey());
-
-            Vector<UserFilePair> list = owner.getFileListObject().getLendList();
-
-            list.add(new UserFilePair(user, file));
-
-            for (UserFilePair ufp : list) {
-                w.writeLine(ufp.compile());
-            }
-
-            w.close();
+            this.remarkToOwner(user, file);
         } catch (Exception e) {
             throw new DatabaseException("Kann Recht nicht speichern!", e);
+        } finally {
+            this.shutdown();
         }
-
-        this.shutdown();
     }
 
+    /**
+     * Ermittelt ob ein Benutzer mit dem angegebenen Namen in der Datenbank existiert.
+     * 
+     * @param name
+     *            Der Name des Benutzers.
+     * @return Gibt <code>true</code> zurück, wenn der Benutzer existiert, andernfalls <code>false</code>.
+     */
     public boolean hasUser(final String name) {
-        return this.users.containsKey(name.toLowerCase());
+        return this.getUserMap().containsKey(name.toLowerCase());
     }
 
     /**
@@ -164,16 +190,24 @@ public final class Database extends DatabaseModel {
         DatabaseStructure.LOCK.lock();
 
         try {
-            Database.instance = null;
+            Database.killInstance();
         } finally {
             DatabaseStructure.LOCK.unlock();
         }
     }
 
-    void addFile(final User user, final FileItem item) throws DatabaseException {
+    /**
+     * Fügt ein {@link FileItem} zur Datenbank hinzu.
+     * 
+     * @param item
+     *            Das {@link FileItem}, das hinzugefügt werden soll.
+     * @throws DatabaseException
+     *             Wird geworfen, wenn das {@link FileItem} nicht hinzugefügt werden konnte.
+     */
+    void addFileItem(final FileItem item) throws DatabaseException {
         try {
             // in Dateitabelle des Benutzers Eintrag schreiben
-            DatabaseModel.updateUserTables(user, item);
+            DatabaseModel.updateUserTables(item);
 
             // in globale Dateitablle Eintrag schreiben
             DatabaseModel.updateGlobalTable(item);
@@ -182,6 +216,46 @@ public final class Database extends DatabaseModel {
         } catch (Exception e) {
             throw new DatabaseException("Die Datei kann nicht hinzugefügt werden!", e);
         }
+    }
+
+    /**
+     * Fügt das Nutzungsrecht in der Access-Tabelle des {@link User} hinzu.
+     * 
+     * @param user
+     *            Der Benutzer.
+     * @param file
+     *            Die Datei.
+     * @throws UtilsException
+     *             Wird geworfen, wenn die Tabelle nicht geschrieben werden konnte.
+     */
+    private void appendToUser(final User user, final FileItem file) throws UtilsException {
+        RSAAppender.appendLine(Tables.AccessTable.getFilename(user), user.getPublicKey(), file.getId()
+                + DatabaseModel.SEPARATOR + Utils.toHexString(file.getKey()));
+    }
+
+    /**
+     * Merkt sich beim Besitzer der Datei, wohin er die Datei ausgeliehen hat.
+     * 
+     * @param user
+     *            Der Besitzer der Datei.
+     * @param file
+     *            Die Datei.
+     * @throws IOException
+     *             Wird geworfen, wenn die Tabelle nicht gespeichert werden konnte.
+     */
+    private void remarkToOwner(final User user, final FileItem file) throws IOException {
+        User owner = file.getOwner();
+        AESWriter w = AESWriter.createWriter(Tables.LendTable.getFilename(owner), owner.getCryptKey());
+
+        Vector<UserFilePair> list = owner.getFileListObject().getLendList();
+
+        list.add(new UserFilePair(user, file));
+
+        for (UserFilePair ufp : list) {
+            w.writeLine(ufp.compile());
+        }
+
+        w.close();
     }
 
 }
