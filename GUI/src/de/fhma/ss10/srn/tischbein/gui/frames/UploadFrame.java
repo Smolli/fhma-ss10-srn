1ff1aad45a5d3c40240888d1d956d63952108be7
kaczmarek.christian <kaczmarek.christian@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 12:11:34 +0000

    UploadFrame angelegt

--- GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
+++ GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
@@ -1,26 +1,31 @@
-package de.fhma.ss10.srn
-.tischbein.gui.frames;
+package de.fhma.ss10.srn.tischbein.gui.frames;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.actions.SearchAction;
 import de.fhma.ss10.srn.tischbein.gui.forms.UploadForm;
 
-public class UploadFrame extends UploadForm {
+public class UploadFrame extends UploadForm implements ActionListener {
 
 	private User user;
-	
-	public UploadFrame()
-	{
-		this.searchButton.setAction(new SearchAction());
-		this.abbortButton.setAction(null);
-		this.saveButton.setAction(null);
-		this.secretField.setAction(null);
-		
-		
-		
+
+	public UploadFrame() {
+		this.searchButton.addActionListener(this);
+		this.abbortButton.addActionListener(this);
+		this.saveButton.addActionListener(this);
 	}
-	
-	
-	
-	
+
+	@Override
+	public void actionPerformed(ActionEvent button) {
+		if (button.getSource() == searchButton) {
+
+		} else if (button.getSource() == saveButton) {
+
+		} else if (button.getSource() == abbortButton) {
+
+		}
+	}
+
 }
