9d855d3b7c04350d664955a4d64c7df8af3c29da
kaczmarek.christian <kaczmarek.christian@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-21 12:57:40 +0000

    SearchButton mit leben gefüllt

--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/UploadAction.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/actions/UploadAction.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 
+import de.fhma.ss10.srn.tischbein.core.db.User;
+import de.fhma.ss10.srn.tischbein.gui.frames.UploadFrame;
+
 /**
  * Upload-Action.
  * 
@@ -12,22 +15,27 @@ import javax.swing.Action;
  */
 public final class UploadAction extends AbstractAction {
 
-    /** Serial UID. */
-    private static final long serialVersionUID = 7077305932498378892L;
+	/** Serial UID. */
+	private static final long serialVersionUID = 7077305932498378892L;
 
-    @Override
-    public void actionPerformed(final ActionEvent arg0) {
-        // TODO Auto-generated method stub
+	@Override
+	/*
+	 * Neues UploadFrame erstellen
+	 */
+	public void actionPerformed(final ActionEvent arg0) {
+		UploadFrame frame = new UploadFrame();
 
-    }
+		frame.setVisible(true);
 
-    @Override
-    public Object getValue(final String key) {
-        if (key.equals(Action.NAME)) {
-            return "Hochladen";
-        } else {
-            return super.getValue(key);
-        }
-    }
+	}
+
+	@Override
+	public Object getValue(final String key) {
+		if (key.equals(Action.NAME)) {
+			return "Hochladen";
+		} else {
+			return super.getValue(key);
+		}
+	}
 
 }
--- trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
+++ trunk/GUI/src/de/fhma/ss10/srn/tischbein/gui/frames/UploadFrame.java
@@ -2,26 +2,46 @@ package de.fhma.ss10.srn.tischbein.gui.frames;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
 
 import de.fhma.ss10.srn.tischbein.core.db.User;
 import de.fhma.ss10.srn.tischbein.gui.forms.UploadForm;
 
 public class UploadFrame extends UploadForm implements ActionListener {
 
-	private User user;
-
 	public UploadFrame() {
 		this.searchButton.addActionListener(this);
 		this.abbortButton.addActionListener(this);
 		this.saveButton.addActionListener(this);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent button) {
+		/*
+		 * Beim Klicken auf den searchbutton öffnet sich ein neues Filechooser
+		 * fenster in dem die Datei ausgewählt werden kann (nur Textdateien)
+		 */
 		if (button.getSource() == searchButton) {
 
-		} else if (button.getSource() == saveButton) {
+			// Create a file chooser
+			final JFileChooser fc = new JFileChooser();
+			// In response to a button click:
+			fc.showOpenDialog(searchButton);
 
+			// Datei Objekt "laden"
+			File file = fc.getSelectedFile();
+			// Namensfeld auf Pfad der Datei setzen
+			this.filenameField.setText(file.getPath());
+			
+			
+		} else if (button.getSource() == saveButton) {
+			
+			//Läd den Pfad aus dem filenameField
+			File file = new File(this.filenameField.getText());
+			
 		} else if (button.getSource() == abbortButton) {
 
 		}
