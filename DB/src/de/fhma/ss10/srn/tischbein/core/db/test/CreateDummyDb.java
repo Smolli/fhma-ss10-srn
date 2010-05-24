b1f2555bcbf704186070ee00d5828ce2d55b934a
Smolli <Smolli@f674c8ac-121b-e304-c274-a3137debf427>
2010-05-24 17:07:02 +0000

    Erstellen der Dummy-DB aufgebohrt.

--- DB/src/de/fhma/ss10/srn/tischbein/core/db/test/CreateDummyDb.java
+++ DB/src/de/fhma/ss10/srn/tischbein/core/db/test/CreateDummyDb.java
@@ -1,6 +1,8 @@
 package de.fhma.ss10.srn.tischbein.core.db.test;
 
 import de.fhma.ss10.srn.tischbein.core.db.Database;
+import de.fhma.ss10.srn.tischbein.core.db.DatabaseException;
+import de.fhma.ss10.srn.tischbein.core.db.FileItem;
 import de.fhma.ss10.srn.tischbein.core.db.User;
 
 /**
@@ -18,21 +20,35 @@ public class CreateDummyDb {
      */
     public static void main(final String[] args) {
         try {
-            Database db = Database.getInstance();
+            CreateDummyDb.createUsers();
 
-            if (!db.hasUser("Susi")) {
-                db.createUser("Susi", "1234");
-            }
-
-            User user = db.getUser("Susi");
+            User user = Database.getInstance().getUser("Susi");
 
             user.unlock("1234");
 
-            user.addFile("Testdatei.txt");
+            FileItem file = user.addFile("Testdatei.txt");
             user.addFile("Noch eine Datei.txt");
             user.addFile("letzte Datei.txt");
+
+            user.getFileListObject().setAccess(Database.getInstance().getUser("franz"), file, true);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Erstellt die Protagonisten, wenn sie noch nicht existieren.
+     * 
+     * @throws DatabaseException
+     *             Wird geworfen, wenn die Benutzer nicht erstellt werden konnten.
+     */
+    private static void createUsers() throws DatabaseException {
+        if (!Database.getInstance().hasUser("susi")) {
+            Database.getInstance().createUser("Susi", "1234");
+        }
+
+        if (!Database.getInstance().hasUser("franz")) {
+            Database.getInstance().createUser("Franz", "1234");
         }
     }
 
